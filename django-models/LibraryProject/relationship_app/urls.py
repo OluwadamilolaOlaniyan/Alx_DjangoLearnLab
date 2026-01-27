55f4e31d19b1ae017b9abe84b81f1bed1655204b
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-01-27 23:23:57 +0100

    user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
+
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(
-        template_name="relationship_app/login.html"
-    ), name="login"),
-
-    path("logout/", auth_views.LogoutView.as_view(
-        template_name="relationship_app/logout.html"
-    ), name="logout"),
-
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout",
+    ),
     path("register/", views.register, name="register"),
 ]
