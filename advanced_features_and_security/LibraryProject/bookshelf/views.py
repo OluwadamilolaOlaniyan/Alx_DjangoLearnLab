ddfd38b1343b32fcc96bb5ba8d12f9b57af3f82d
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-04 21:15:32 +0100

    Custom User Model Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create_book(request):
+    return render(request, "bookshelf/create_book.html")
+
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_book(request):
+    return render(request, "bookshelf/delete_book.html")
+
+
+from django.shortcuts import render
+from .forms import ExampleForm
+
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the data securely
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            # For now, just render success
+            return render(request, 'bookshelf/form_example.html', {'form': form, 'success': True})
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
