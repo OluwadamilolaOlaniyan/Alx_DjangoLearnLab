651c40f0dbe59cbe33b30b8191597bfe6fa19ad8
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-05 03:22:16 +0100

    API END POINT WITH DJANGO REST

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
 
-from django.urls import path
-from .views import BookList
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
+
+    path('', include(router.urls)),
 ]
