dc1108b667c74de17bbf120da73a93f8aa1d8de1
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-05 03:32:12 +0100

    API END POINT WITH DJANGO REST

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 
-from rest_framework import generics 
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
