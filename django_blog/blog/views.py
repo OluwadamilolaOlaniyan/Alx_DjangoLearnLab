8b194594912c32ae2a8e58fd1328f7ab62231853
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-15 19:28:08 +0100

    added login_required decorator import to views file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
