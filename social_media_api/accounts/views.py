062cb4aa67b9bf6dcc28d7fa3afa69c2d07320cc
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-28 06:14:03 +0100

    Custom User Model, Token Authentication

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,29 @@
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer, LoginSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email,
+            "bio": user.bio
+        })
