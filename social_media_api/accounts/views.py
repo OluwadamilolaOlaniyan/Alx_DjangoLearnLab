dae42588d2ef35d2aba7524673cb00d067de683e
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-28 07:02:10 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, LoginSerializer
-
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -27,3 +28,30 @@ class ProfileView(APIView):
             "email": user.email,
             "bio": user.bio
         })
+
+User = get_user_model()
+
+
+class FollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(User, id=user_id)
+
+        if request.user == user_to_follow:
+            return Response({"error": "You cannot follow yourself."}, status=400)
+
+        request.user.following.add(user_to_follow)
+
+        return Response({"message": f"You are now following {user_to_follow.username}"})
+
+
+class UnfollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(User, id=user_id)
+
+        request.user.following.remove(user_to_unfollow)
+
+        return Response({"message": f"You unfollowed {user_to_unfollow.username}"})
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-from .models import Post, Comment
+from .models import Post
+from .models import Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.views import APIView
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.response import Response
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -25,3 +28,15 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        followed_users = request.user.following.all()
+        posts = Post.objects.filter(
+            author__in=followed_users
+        ).order_by('-created_at')
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
