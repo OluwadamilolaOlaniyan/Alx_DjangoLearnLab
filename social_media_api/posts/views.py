99acf4093d56db5e386af1d2510f5420920012c3
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-28 07:38:00 +0100

    update

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,24 +1,49 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-from .models import Post
-from .models import Comment
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.views import APIView
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.response import Response
-
+from notifications.models import Notification
+from rest_framework.decorators import action
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    filter_backends = [SearchFilter]
-    search_fields = ['title', 'content']
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post'], permission_classes=[permissions.IsAuthenticated])
+    def like(self, request, pk=None):
+        post = generics.get_object_or_404(Post, pk=pk)
+
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+
+        if not created:
+            return Response({"message": "You already liked this post."})
+
+        if post.author != request.user:
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                target=post.title
+            )
+
+        return Response({"message": "Post liked successfully."})
+
+    @action(detail=True, methods=['post'], permission_classes=[permissions.IsAuthenticated])
+    def unlike(self, request, pk=None):
+        post = generics.get_object_or_404(Post, pk=pk)
+
+        Like.objects.filter(user=request.user, post=post).delete()
+
+        return Response({"message": "Post unliked successfully."})
 
 
 class CommentViewSet(viewsets.ModelViewSet):
